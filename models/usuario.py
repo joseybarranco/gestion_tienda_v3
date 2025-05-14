9374df1b5347a647c7e0742100a40ed77a6f6c56
joseybarranco <joseyenred@hotmail.com>
2025-05-14 19:22:35 +0200

    Creo la clase usuarios

---
+++ models/usuario.py
@@ -0,0 +1,6 @@
+class Usuario:
+    def __init__(self, nombre,  email, contraseña, fecha_registro ):
+        self.nombre = nombre
+        self.email = email
+        self.contraseña = contraseña
+        self.fecha_registro = fecha_registro
